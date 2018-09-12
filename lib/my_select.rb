8b0dbfc5adc95d9407a6fee67d99b8a52b2f1bcb
patrickp246@gmail.com <patrickp246@gmail.com>
2018-09-12 18:24:23 +0000

    automatically backed up by learn

--- lib/my_select.rb
+++ lib/my_select.rb
@@ -1,6 +1,12 @@
 def my_select(collection)
  counter = 0
+ rtn_array = []
  if collection.count !== 0 
-   while counter < collection.count do 
+   while counter < collection.count do
+     if yield(collection[counter])
+       rtn_array << collection[counter]
+     end
+   end
  end
+ rtn_array
 end
